8ec905fd0ac432d34234e2eb686efc43c36bcf8c
Spazapy <n.marcel.1994@gmx.de>
2025-05-28 12:05:18 +0200

    update

--- ProductivityHub/Controllers/HomeController.cs
+++ ProductivityHub/Controllers/HomeController.cs
@@ -16,9 +16,52 @@ public class HomeController : Controller
     }
     public IActionResult Index()
     {
+        
         return View();
     }
 
+    public IActionResult Search(string word)
+    {
+        var games = _db.Games;
+        var series = _db.Series;
+        var tasks = _db.TaskItems;
+        var searchgames = from m in games select m;
+        var searchseries = from m in series select m;
+        var searchtasks = from m in tasks select m;
+
+        if (!String.IsNullOrEmpty(word))
+        {
+            var lowerWord = word.ToLower();
+            searchgames = searchgames.Where(s => s.Title.Contains(lowerWord)
+                                                 || s.Platform.Contains(lowerWord)
+                                                 || s.Genre.Contains(lowerWord)
+                                                 || s.Notes.Contains(lowerWord)
+                                                 || s.Status.Contains(lowerWord)
+                                                 || s.UserId.Contains(lowerWord));
+            searchseries = searchseries.Where(s => s.Title.Contains(lowerWord)
+                                                   || s.Platform.Contains(lowerWord)
+                                                   || s.Status.Contains(lowerWord)
+                                                   || s.UserId.Contains(lowerWord));
+            searchtasks = searchtasks.Where(s => s.Title.Contains(lowerWord)
+                                                 || s.Description.Contains(lowerWord)
+                                                 || s.Status.Contains(lowerWord)
+                                                 || s.Priority.Contains(lowerWord)
+                                                 || s.UserId.Contains(lowerWord));
+            var viewModel = new SearchResults
+            {
+                Games = searchgames.ToList(),   // Materialize query results
+                Series = searchseries.ToList(), // Materialize query results
+                Tasks = searchtasks.ToList()    // Materialize query results
+            };
+            return View(viewModel);   
+        }
+        return View();
+    }
+
+    public IActionResult Details()
+    {
+        return View();
+    }
     public IActionResult Spiele()
     {
         var games = _db.Games;
---
+++ ProductivityHub/Models/SearchResults.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductivityHub.Models;
+
+public class SearchResults
+{
+    public IEnumerable<Game> Games { get; set; }
+    public IEnumerable<Series> Series { get; set; }
+    public IEnumerable<TaskItem> Tasks { get; set; }
+}
